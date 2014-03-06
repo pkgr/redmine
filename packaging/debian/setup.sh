9c8d33dce1ade9fc69fdb7f2899ec2020b9ed10f
Cyril Rohr <cyril.rohr@gmail.com>
2014-03-06 22:57:18 +0000

    Make sure to have a postgres adapter in database.yml

--- packaging/debian/setup.sh
+++ packaging/debian/setup.sh
@@ -5,3 +5,14 @@ set -ex
 for file in config/*.yml.example; do
   cp $file config/$(basename $file .example)
 done
+
+
+cat > config/database.yml <<EOF
+# PostgreSQL configuration example
+production:
+  adapter: postgresql
+  database: redmine
+  host: localhost
+  username: postgres
+  password: "postgres"
+EOF
