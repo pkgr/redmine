89839ed6f0b08cff2d59de53801b913941277fa0
Cyril Rohr <cyril.rohr@gmail.com>
2014-03-12 10:53:16 +0000

    Remove useless setup.

--- packaging/debian/setup.sh
+++ packaging/debian/setup.sh
@@ -6,15 +6,8 @@ for file in config/*.yml.example; do
   cp $file config/$(basename $file .example)
 done
 
-
 cat > config/database.yml <<EOF
 # PostgreSQL configuration example
 production:
   adapter: postgresql
-  database: redmine
-  host: localhost
-  username: postgres
-  password: "postgres"
 EOF
-
-sudo service postgresql start
